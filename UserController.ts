0ce9255365aeeccc92ccfa6613fdfe7278d2c53c
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-12-25 14:07:21 -0500

    indentation

--- UserController.ts
+++ UserController.ts
@@ -3,29 +3,29 @@ import { Controller, Param, Body, Get, Post, Put, Delete } from 'routing-control
 @Controller()
 export class UserController {
   
-  @Get('/users')
-  getAll() {
-    return 'Returning all users';
-  }
+    @Get('/users')
+    getAll() {
+        return 'Returning all users';
+    }
 
-  @Get('/users/:id')
-  getOne(@Param('id') id: number) {
-    return `Returning user with id ${ id }`;
-  }
+    @Get('/users/:id')
+    getOne(@Param('id') id: number) {
+        return `Returning user with id ${ id }`;
+    }
 
-  @Post('/users')
-  post(@Body() user: any) {
-    return 'Saving user';
-  }
+    @Post('/users')
+    post(@Body() user: any) {
+        return 'Saving user';
+    }
 
-  @Put('/users/:id')
-  put(@Param('id') id: number, @Body() user: any) {
-    return `Updating user with id ${ id }`;
-  }
+    @Put('/users/:id')
+    put(@Param('id') id: number, @Body() user: any) {
+        return `Updating user with id ${ id }`;
+    }
 
-  @Delete('/users/:id')
-  remove(@Param('id') id: number) {
-    return `Deleting user with id ${ id }`;
-  }
+    @Delete('/users/:id')
+    remove(@Param('id') id: number) {
+        return `Deleting user with id ${ id }`;
+    }
 
 }
--- app.ts
+++ app.ts
@@ -3,9 +3,9 @@ import { createExpressServer } from 'routing-controllers';
 import { UserController } from './UserController';
 
 const app = createExpressServer({
-  controllers: [UserController]
+    controllers: [UserController]
 });
 
 app.listen(3000, () => {
-  console.log('app started on port 3000');
+    console.log('app started on port 3000');
 });
