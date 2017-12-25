080393bd84d693645aa131897fabe72b833e0a4c
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-12-25 14:05:06 -0500

    init

---
+++ app.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import { createExpressServer } from 'routing-controllers';
+import { UserController } from './UserController';
+
+const app = createExpressServer({
+  controllers: [UserController]
+});
+
+app.listen(3000, () => {
+  console.log('app started on port 3000');
+});
