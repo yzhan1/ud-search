f4b48c9c9b41b8af4db17b245decfd7fd9be49f9
Yaoming Zhan <zhanyaoming2014@gmail.com>
2018-01-04 01:50:37 -0500

    ..

--- app.ts
+++ app.ts
@@ -11,12 +11,15 @@ const PORT = process.env.PORT || 5000;
 const app = createExpressServer({
     cors: true,
     controllers: [
-        DictionaryController,
-        IndexController
+        DictionaryController
     ]
 });
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req: any, res: any) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const logger = Factory.getLogger('AppLogger');
 
